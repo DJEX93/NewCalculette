4fb9bdda6ddbfc425799c4d2af5772ece9d063b4
DJEX93 <DJEX93@GMAIL.COM>
2016-03-04 17:30:30 +0200

    boutton add
    Ajout des bouton dans le programme

--- app/src/main/java/com/mhb/newcalculette/MainActivity.java
+++ app/src/main/java/com/mhb/newcalculette/MainActivity.java
@@ -5,8 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button buttonPlus;
+    Button buttonMoins;
+    Button buttonDiv;
+    Button buttonMul;
+    Button buttonC;
+    Button buttonEgal;
+    Button buttonPoint;
+    Button buttonDel;
+    Button buttonPM;
+    Button buttonPC;
+    EditText ecran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +38,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        buttonMoins = (Button) findViewById(R.id.buttonMoins);
+        buttonDiv = (Button) findViewById(R.id.buttonDiv);
+        buttonMul = (Button) findViewById(R.id.buttonMul);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonEgal = (Button) findViewById(R.id.buttonEgal);
+        buttonPoint = (Button) findViewById(R.id.buttonPoint);
+        buttonDel = (Button) findViewById(R.id.buttonDel);
+        buttonPM = (Button) findViewById(R.id.buttonPM);
+        buttonPC = (Button) findViewById(R.id.buttonPC);
+        button0 = (Button) findViewById(R.id.button0);
+        ecran = (EditText) findViewById(R.id.ecran);
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
