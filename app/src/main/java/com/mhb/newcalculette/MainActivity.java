2453dd3c732de38275138380804a5e6bff191712
DJEX93 <DJEX93@GMAIL.COM>
2016-03-04 21:45:55 +0200

    Calculatrice Fonctionelle
    Calculatrice Fonctionelle  mais reste bcq de chose a coriger

--- app/src/main/java/com/mhb/newcalculette/MainActivity.java
+++ app/src/main/java/com/mhb/newcalculette/MainActivity.java
@@ -5,8 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +31,13 @@ public class MainActivity extends AppCompatActivity {
     Button buttonDel;
     Button buttonPM;
     Button buttonPC;
-    EditText ecran;
+    TextView ecran;
+
+
+    double chiffre1;
+    boolean clicOperateur = false;
+    boolean update = false;
+    String operateur = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,8 +66,255 @@ public class MainActivity extends AppCompatActivity {
         buttonDel = (Button) findViewById(R.id.buttonDel);
         buttonPM = (Button) findViewById(R.id.buttonPM);
         buttonPC = (Button) findViewById(R.id.buttonPC);
-        button0 = (Button) findViewById(R.id.button0);
-        ecran = (EditText) findViewById(R.id.ecran);
+        ecran = (TextView) findViewById(R.id.ecran);
+
+
+
+
+
+
+        View.OnClickListener Point = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick(".");
+
+            }
+
+        };
+
+
+
+        View.OnClickListener chiffre0 = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("0");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("1");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("2");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre3 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("3");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre4 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("4");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre5 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("5");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre6 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("6");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre7 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("7");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre8 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("8");
+
+            }
+
+        };
+
+        View.OnClickListener chiffre9 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                ChiffreClick("9");
+
+            }
+
+        };
+
+        View.OnClickListener Plus = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                plusClick();
+
+            }
+
+        };
+
+        View.OnClickListener Moins = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                moinsClick();
+
+            }
+
+        };
+
+        View.OnClickListener Div = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                divClick();
+
+            }
+
+        };
+
+        View.OnClickListener Mul = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                mulClick();
+
+            }
+
+        };
+
+        View.OnClickListener Clear = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                resetClick();
+
+            }
+
+        };
+
+        View.OnClickListener Egal = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                egalClick();
+
+            }
+
+        };
+
+        View.OnClickListener Del = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                resetClick();
+
+            }
+
+        };
+
+        View.OnClickListener PM = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                //TODO trouver la bonne formule
+                egalClick();
+
+            }
+
+        };
+
+        View.OnClickListener PC = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action du bouton chiffre
+                //TODO trouver la bonne formule
+                egalClick();
+
+            }
+
+        };
+
+
+
+
+
+
+
+
+
+
+
+        button0.setOnClickListener(chiffre0);
+        button1.setOnClickListener(chiffre1);
+        button2.setOnClickListener(chiffre2);
+        button3.setOnClickListener(chiffre3);
+        button4.setOnClickListener(chiffre4);
+        button5.setOnClickListener(chiffre5);
+        button6.setOnClickListener(chiffre6);
+        button7.setOnClickListener(chiffre7);
+        button8.setOnClickListener(chiffre8);
+        button9.setOnClickListener(chiffre9);
+        buttonPlus.setOnClickListener(Plus);
+        buttonMoins.setOnClickListener(Moins);
+        buttonDiv.setOnClickListener(Div);
+        buttonMul.setOnClickListener(Mul);
+        buttonC.setOnClickListener(Clear);
+        buttonEgal.setOnClickListener(Egal);
+        buttonPoint.setOnClickListener(Point);
+        buttonDel.setOnClickListener(Del);
+        buttonPM.setOnClickListener(PM);
+        buttonPC.setOnClickListener(PC);
+
+
+
+
+
+
 
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -72,6 +326,123 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
+    //voici la méthode qui est exécutée lorsque l'on clique sur un bouton chiffre
+
+    public void ChiffreClick (String str) {
+        if(update){
+            update = false;
+        }else{
+            if(!ecran.getText().equals("0"))
+                str = ecran.getText() + str;
+        }
+        ecran.setText(str);
+    }
+
+    //voici la méthode qui est  exécutée lorsque l'on clique sur le bouton +
+    public void plusClick(){
+
+        if(clicOperateur){
+            calcul();
+            ecran.setText(String.valueOf(chiffre1));
+        }else{
+            chiffre1 = Double.valueOf(ecran.getText().toString()).doubleValue();
+            clicOperateur = true;
+        }
+        operateur = "+";
+        update = true;
+    }
+
+    //Méthode qui est  exécutée lorsque l'on clique sur le bouton -
+    public void moinsClick (){
+        if(clicOperateur){
+            calcul();
+            ecran.setText(String.valueOf(chiffre1));
+        }else{
+            chiffre1 = Double.valueOf(ecran.getText().toString()).doubleValue();
+            clicOperateur = true;
+        }
+        operateur = "-";
+        update = true;
+    }
+
+    //Méthode qui est  exécutée lorsque l'on clique sur le bouton *
+    public void mulClick(){
+        if(clicOperateur){
+            calcul();
+            ecran.setText(String.valueOf(chiffre1));
+        }else{
+            chiffre1 = Double.valueOf(ecran.getText().toString()).doubleValue();
+            clicOperateur = true;
+        }
+        operateur = "*";
+        update = true;
+    }
+
+    //Méthode qui est  exécutée lorsque l'on clique sur le bouton /
+    public void divClick (){
+        if(clicOperateur){
+            calcul();
+            ecran.setText(String.valueOf(chiffre1));
+        }else{
+            chiffre1 = Double.valueOf(ecran.getText().toString()).doubleValue();
+            clicOperateur = true;
+        }
+        operateur = "/";
+        update = true;
+    }
+
+    //voici la méthode qui est  exécutée lorsque l'on clique sur le bouton =
+    public void egalClick (){
+        calcul();
+        update = true;
+        clicOperateur = false;
+    }
+
+    //voici la méthode qui est  exécutée lorsque l'on clique sur le bouton C
+    public void resetClick (){
+        clicOperateur = false;
+        update = true;
+        chiffre1 = 0;
+        operateur = "";
+        ecran.setText("");
+    }
+
+    //Voici la méthode qui fait le calcul qui a été demandé par l'utilisateur
+    private void calcul(){
+        if(operateur.equals("+")){
+            chiffre1 = chiffre1 + Double.valueOf(ecran.getText().toString()).doubleValue();
+            ecran.setText(String.valueOf(chiffre1));
+        }
+
+
+
+        if(operateur.equals("-")){
+            chiffre1 = chiffre1 - Double.valueOf(ecran.getText().toString()).doubleValue();
+            ecran.setText(String.valueOf(chiffre1));
+        }
+
+        if(operateur.equals("*")){
+            chiffre1 = chiffre1 * Double.valueOf(ecran.getText().toString()).doubleValue();
+            ecran.setText(String.valueOf(chiffre1));
+        }
+
+        if(operateur.equals("/")){
+            try{
+                chiffre1 = chiffre1 / Double.valueOf(ecran.getText().toString()).doubleValue();
+                ecran.setText(String.valueOf(chiffre1));
+            }catch(ArithmeticException e){
+                ecran.setText("0");
+            }
+        }
+
+
+
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
